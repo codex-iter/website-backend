3f2a497807896c16567b5c93ba0cd00356afa4f0
gymy <abhisonuk123@gmail.com>
2021-08-08 13:44:30 +0530

    fix : used library for CORS

--- app.py
+++ app.py
@@ -1,34 +1,22 @@
 from flask import Flask, make_response, request
 from model.event import Event
+from flask_cors import CORS
 
 app = Flask(__name__)
 # TODO: setup cors with proper resources matching frontend
-app.config['CORS_HEADERS'] = 'Content-Type'
-
-
-def _build_cors_prelight_response():
-    response = make_response()
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add("Access-Control-Allow-Headers", "*")
-    response.headers.add("Access-Control-Allow-Methods", "*")
-    return response
+CORS(app)
 
 
 @app.route('/', methods=['GET'])
 def index():
     return 'webserver is working fine'
 
-
 @app.route('/api/getEvents', methods=['GET', 'OPTIONS'])
-def event():
-    if request.method == 'OPTIONS':
-        return _build_cors_prelight_response()
+def getEvent():
     if request.method == 'GET':
         events = Event()
         res = make_response(events.get_all_events())
-        res.headers["Access-Control-Allow-Origin"] = "*"
         return res
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- model/event.py
+++ model/event.py
@@ -72,16 +72,3 @@ class Event:
         cursors = self.collection.find({})
         docs = dumps(cursors, indent=2)
         return docs
-
-
-# Testing
-
-# cats = ['../testingAssets/cat 1.jpeg','../testingAssets/cat 3.jpeg']
-# ev = Event()
-# _title = "Event joey"
-# _des = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rutrum hendrerit consectetur. Aliquam tincidunt nisi in metus sodales cursus. Etiam sed dui feugiat, volutpat mauris sit amet, auctor nulla. Vivamus iaculis metus lobortis tortor viverra, sed venenatis metus sodales. Proin bibendum auctor aliquam. Integer blandit dolor lectus, ut aliquet diam sagittis eu. Donec sodales, justo nec sollicitudin pharetra, sem eros finibus sapien, in sollicitudin urna neque id arcu. Phasellus malesuada lectus felis, at dictum neque feugiat eu. Duis ut aliquam nibh, a porttitor justo. "
-# _images = cats
-# _date = str(datetime.datetime.now().date())
-# _organizers = ["joey", "chandler", "rachel", "ross"]
-# print(ev.set_event(_title, _des, _date, _organizers, _images))
-# print(ev.get_all_events())
